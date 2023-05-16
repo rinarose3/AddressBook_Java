9f5745742ebc2a5f3baa8105b77f2880a9d0bf3d
rinarose3 <ryzhkova_ie@mail.ru>
2023-05-16 19:02:34 +0300

    Запуск сервера

---
+++ src/main/java/git/rinarose3/abjava/AbjavaApplication.java
@@ -0,0 +1,13 @@
+package git.rinarose3.abjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbjavaApplication.class, args);
+	}
+
+}
