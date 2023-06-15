8acccbbd9559255ab6206a6143a82e5c9048dfef
rinarose3 <ryzhkova_ie@mail.ru>
2023-06-15 17:29:55 +0300

    Версия:v2 Улучшенный интерфейс представляющий репозиторий для доступа к данным объектов AddressBook в базе данных.

--- src/main/java/git/rinarose3/abjava/repository/AddressBookRepository.java
+++ src/main/java/git/rinarose3/abjava/repository/AddressBookRepository.java
@@ -1,5 +1,6 @@
-package git.rinarose3.abjava;
+package git.rinarose3.abjava.repository;
 
+import git.rinarose3.abjava.models.AddressBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
