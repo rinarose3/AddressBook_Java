38c95e17739a19b2b1fd6321a9ee00a77ac5fc64
rinarose3 <ryzhkova_ie@mail.ru>
2023-06-15 17:41:25 +0300

    Версия:v1 Интерфейс представляющий репозиторий для доступа к данным пользователя

---
+++ src/main/java/git/rinarose3/abjava/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package git.rinarose3.abjava.repository;
+
+import git.rinarose3.abjava.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "select * from users where name = ?1", nativeQuery = true)
+    Optional<User> findUserByName(String name);
+}
