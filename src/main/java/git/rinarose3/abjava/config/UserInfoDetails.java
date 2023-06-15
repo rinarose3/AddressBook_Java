bb63d454606d76cc19ab56e5e6908200dfce4783
rinarose3 <ryzhkova_ie@mail.ru>
2023-06-15 17:44:17 +0300

    Версия:v1 Реализация интерфейса UserDetails из фраймворка Spring Security.

---
+++ src/main/java/git/rinarose3/abjava/config/UserInfoDetails.java
@@ -0,0 +1,63 @@
+package git.rinarose3.abjava.config;
+
+import git.rinarose3.abjava.models.User;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data //геттеры и сеттеры
+public class UserInfoDetails implements UserDetails {
+
+    private String name;
+    private String password;
+    private List<GrantedAuthority> roles;
+
+    public UserInfoDetails(User user) {
+        this.name = user.getName();
+        this.password = user.getPassword();
+        this.roles = Arrays.stream(user.getRole().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
