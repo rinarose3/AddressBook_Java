78a0ab410ac3f17ee3501804fcf446980be04f5c
rinarose3 <ryzhkova_ie@mail.ru>
2023-06-15 17:26:43 +0300

    Версия:v2 Улучшенный контроллер адресной книги с добавленными аннотациями для описания операции в документации OpenAPI

--- src/main/java/git/rinarose3/abjava/controllers/AddressBookController.java
+++ src/main/java/git/rinarose3/abjava/controllers/AddressBookController.java
@@ -1,30 +1,42 @@
-package git.rinarose3.abjava;
+package git.rinarose3.abjava.controllers;
 
 
+import git.rinarose3.abjava.service.AddressBookService;
+import git.rinarose3.abjava.models.AddressBook;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/ablist/")
+@Tag(name = "Address Book", description = "API для управления списком адресной книги")
 public class AddressBookController {
 
     @Autowired
     private AddressBookService addressBookService;
 
     @GetMapping
+    //@Tag(name = "Address Book", description = "API для получения списка контактов в адресной книге")
+    @Operation(summary = "Получить список контактов в адресной книге")
     public List<AddressBook> getPeople() {
         return addressBookService.getAllAddressBook();
     }
 
     @PostMapping
+    //@Tag(name = "Address Book", description = "API для создания контакта в адресной книге")
+    @Operation(summary = "Создать контакт в адресной книге")
     public ResponseEntity<AddressBook> postPeople(@RequestBody AddressBook addressBook) {
         return ResponseEntity.ok(addressBookService.saveAddressBook(addressBook));
     }
 
     @PutMapping
+    //@Tag(name = "Address Book", description = "API для обновления контакта в адресной книге")
+    @Operation(summary = "Обновить контакт в адресной книге")
     public ResponseEntity<AddressBook> putPeople(@RequestBody AddressBook addressBook) {
         try {
             return ResponseEntity.ok(addressBookService.updateAddressBook(addressBook));
@@ -34,6 +46,8 @@ public class AddressBookController {
     }
 
     @DeleteMapping
+    //@Tag(name = "Address Book", description = "API для удаления контакта в адресной книге")
+    @Operation(summary = "Удалить контакт в адресной книге")
     public ResponseEntity<String> deletePeople(@RequestParam("id") Long id) {
         try {
             addressBookService.deleteAddressBookById(id);
